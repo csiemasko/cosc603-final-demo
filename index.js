e3e2a4894097695b0c83bd6eb08c1997576e9907
Blake Joynes <blakejoynes@gmail.com>
2018-05-19 11:45:04 -0400

    change package.json

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express()
+var port = process.env.PORT || 8080;
+
+
+
 app.use(express.static('dist'))
 app.get('/', (req,res) => {
     res.sendFile(`${__dirname}/dist/index.html`)
 });
-app.listen(8081)
+app.listen(port)
 
 
